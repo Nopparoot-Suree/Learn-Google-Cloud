f7385d98b72439695e8052eb2f5a335267f139ed
RhythmX1 <86911729+Nopparoot-Suree@users.noreply.github.com>
2022-10-02 23:28:42 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const vision = require('@google-cloud/vision') //เรียกใช้ google vision Libary
 const textToSpeech = require('@google-cloud/text-to-speech'); //เรียกใช้ google text-to-speech Libary
 
-//เอาข้อมูล Json ใน service account.json มาใส่ในค่าข้างใน
+//เอาข้อมูล Json ใน service account.json มาใส่ในค่าข้างใน (เป็นการ set ค่า เป็น JSON)
 const CREDENTIALS = JSON.parse(JSON.stringify({
   "type": "",
   "project_id": "",
@@ -16,6 +16,7 @@ const CREDENTIALS = JSON.parse(JSON.stringify({
 }
 ));
 
+//ดึงค่าคงที่ private_key และ client_email จาก CREDENTIALS มาเก็บไว้ในค่าคงที่ CONFIG
 const CONFIG = {
     credentials: {
         private_key: CREDENTIALS.private_key,
@@ -23,8 +24,10 @@ const CONFIG = {
     }
 };
 
+//เรียกใช้ Google Vision และ สร้าง Module ออกไปใช้งานไฟล์อื่นๆ
 exports.client_vision = new vision.ImageAnnotatorClient(CONFIG)
 
+//เรียกใช้ text to speech และ สร้าง Module ออกไปใช้งานไฟล์อื่นๆ
 exports.client_texet_to_speech = new textToSpeech.TextToSpeechClient(CONFIG);
 
 
