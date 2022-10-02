f61bf821add51e2fcd7e5d573cc48a0b2b0d5029
RhythmX1 <86911729+Nopparoot-Suree@users.noreply.github.com>
2022-10-02 23:55:35 +0700

    Update detect_image_to_text.js

--- detect_image_to_text.js
+++ detect_image_to_text.js
@@ -1,16 +1,16 @@
 //วิธีใช้ให้เปิด terminal directory path ของ folder นี้ จากนั้นพิมพ์คำสั่ง node <ชื่อไฟล์ที่ต้องการรัน>
 //เช่น node detect_image_to_text.js
-const {client_vision} = require('./index.js')
+const {client_vision} = require('./index.js') //import module จากไฟล์ index.js
 
 //อ่านไฟล์ภาพเป็นข้อความ
-async function DetectImage(file_path) {
-  const [result] = await client_vision.textDetection(file_path)
+async function DetectImage(file_path) { //สร้าง funtions แบบ Promise
+  const [result] = await client_vision.textDetection(file_path) //เรียกใช้ Google Vision หมวด การตรวจหา ข้อความจากรูปภาพ 
   
-    const detections = result.fullTextAnnotation.text
+    const detections = result.fullTextAnnotation.text //นำผลลัพท์ที่ได้เก็บไว้ในค่าคงที่ detections
 
-    return console.log(detections);
+    return console.log(detections); //แสดงผลลัพท์ที่ได้ออกมา
 }
 
-var image = './image/form.jpg'
+var image = './image/form.jpg'  //path รูปภาพ
 
-DetectImage(image)
+DetectImage(image) //เรียกใช้ function
